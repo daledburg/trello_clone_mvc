ee0a7e0227d4b484366b6669b5e4489f44282f68
Dale Dahlenburg <dale.19@icloud.com>
2022-10-25 10:47:45 +1000

    MVC start

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from db import db, ma
+from controllers.cards_controller import cards_bp
 import os
 
 def create_app():
     app = Flask(__name__)
-
+    app.config ['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-    db = SQLAlchemy(app)
+    db.init_app(app)
+    ma.init_app(app)
+
+    app.register_blueprint(cards_bp)
 
     return app
 
