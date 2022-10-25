97ef30e98c3f2f95aa52a42b0131e567af885434
Dale Dahlenburg <dale.19@icloud.com>
2022-10-25 20:23:15 +1000

    Crud implemented

--- controllers/cards_controller.py
+++ controllers/cards_controller.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from datetime import date
 from db import db
 from models.card import Card, CardSchema
 
@@ -6,16 +7,60 @@ cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
 
 @cards_bp.route('/')
 # @jwt_required()
-def all_cards():
+def get_all_cards():
     # if not authorize():
     #     return {'error': 'You must be an admin'}, 401
 
-    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+    stmt = db.select(Card).order_by(Card.date.desc(), Card.title)
     cards = db.session.scalars(stmt)
     return CardSchema(many=True).dump(cards)
 
 @cards_bp.route('/<int:id>/')
-def one_card(id):
+def get_one_card(id):
     stmt = db.select(Card).filter_by(id=id)
     card = db.session.scalar(stmt)
-    return CardSchema().dump(card)
+    if card:
+        return CardSchema().dump(card)
+    else:
+        return {'error': f'Card not found with id {id}'}, 404
+
+@cards_bp.route('/', methods=['POST'])
+def create_card():
+    # Create a new card model instance from the user_info
+    card = Card(
+        title = request.json['title'],
+        description = request.json['description'],
+        date = date.today(),
+        status = request.json['status'],
+        priority = request.json['priority']
+    )
+    # Add and commit user to DB
+    db.session.add(card)
+    db.session.commit()
+    # Respond to client
+    return CardSchema().dump(card), 201
+
+@cards_bp.route('/<int:id>/', methods=['PUT', 'PATCH'])
+def update_one_card(id):
+    stmt = db.select(Card).filter_by(id=id)
+    card = db.session.scalar(stmt)
+    if card:
+        card.title = request.json.get('title') or card.title
+        card.description = request.json.get('description') or card.description
+        card.status = request.json.get('status') or card.status
+        card.priority = request.json.get('priority') or card.priority
+        db.session.commit()
+        return CardSchema().dump(card)
+    else:
+        return {'error': f'Card not found with id {id}'}, 404
+
+@cards_bp.route('/<int:id>/', methods=['DELETE'])
+def delete_one_card(id):
+    stmt = db.select(Card).filter_by(id=id)
+    card = db.session.scalar(stmt)
+    if card:
+        db.session.delete(card)
+        db.session.commit()
+        return {'message': f"Card '{card.title}' deleted successfully"}
+    else:
+        return {'error': f'Card not found with id {id}'}, 404
--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import os
 
 def create_app():
     app = Flask(__name__)
+
+    @app.errorhandler(404)
+    def not_found(err):
+        return {'error': str(err)}, 404
+
     app.config ['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
